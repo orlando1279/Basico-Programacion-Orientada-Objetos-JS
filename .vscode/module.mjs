52184c6af2a4271ca64e5a2da14697e44a388c36
orlando1279 <108104503+orlando1279@users.noreply.github.com>
2023-09-22 12:00:28 -0500

    iniciando repositorio

---
+++ .vscode/module.mjs
@@ -0,0 +1,14 @@
+import { PlatziClass } from "./main.Js/index.js";s
+const clase67 = new PlatziClass({
+  name: "JavaScript: orientado a objetos, basado en prototipos",
+  videoID: "estevdncbncbcnnccvgdbcncmcmcjvm",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
